8fbdccb87cbcaf70cc4fe2b9cae46db0c33cac7d
Marina Rachlin <Marina.rachlin@gmail.com>
2024-01-26 21:28:11 +0200

    first commit

---
+++ components/layout/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="py-1">
+      <p className="text-center mt-1">
+     ©Copyright 2024 Joblink | Design By <a href="https://www.egenslab.com/">Marina Rachlin</a>
+        <a
+          className="ml-4"
+          rel="noreferrer"
+          target="_blank"
+          href="https://storyset.com/people"
+        >
+          People illustrations by Storyset
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;

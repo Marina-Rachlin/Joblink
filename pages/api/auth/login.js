a8e6875d3a2cb06217f18539b964012ea5275ffb
Marina Rachlin <Marina.rachlin@gmail.com>
2024-01-27 21:07:21 +0200

    login

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -6,6 +6,7 @@ export default async (req, res) => {
     const { username, password } = req.body;
 
     try {
+      console.log(`trying to call  ${process.env.API_URL}/api/token/`)
       const response = await axios.post(
         `${process.env.API_URL}/api/token/`,
         {
@@ -18,6 +19,8 @@ export default async (req, res) => {
           },
         }
       );
+
+      console.log('response of login=>', response)
 
       if (response.data.access) {
         res.setHeader("Set-Cookie", [

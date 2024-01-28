66762cff34297145c68ed9c7249f4ff26b2652d7
Marina Rachlin <Marina.rachlin@gmail.com>
2024-01-28 09:54:41 +0200

    login

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     API_URL: "https://joblink-app-c9b1ba4e0b1c.herokuapp.com",
+    MAPBOX_ACCESS_TOKEN: process.env.MAPBOX_ACCESS_TOKEN,
   },
 };
 

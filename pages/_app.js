0274549f626ba0568fc23a489bb15257d6b7b067
Marina Rachlin <Marina.rachlin@gmail.com>
2024-02-20 18:11:38 +0200

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,26 @@
 import "../styles/globals.css";
 import { AuthProvider } from "../context/AuthContext";
 import { JobProvider } from "../context/JobContext";
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AuthProvider>
+    <>
+    <Script  strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-Q5DYQV3TYT"/>
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'G-Q5DYQV3TYT');`}
+  </Script>
+      <AuthProvider>
       <JobProvider>
         <Component {...pageProps} />
       </JobProvider>
     </AuthProvider>
+    </>
   );
 }
 
